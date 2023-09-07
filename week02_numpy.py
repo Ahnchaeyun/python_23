5a6af65c588425c47561863a9e01124c4dd98703
202130119 <202130119@email.daelim.ac.kr>
2023-09-07 16:30:14 +0900

    1차 수정

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,9 +1,20 @@
 import numpy as np
+import random
 
 v = np.array([
     [1, 3, -9, 2],
     [71, 13, -22, 7]
-    ])
+])
 
-print(v.ndim, v.shape, v.data, v.dtype, v.strudes)
+print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+
+number = list(map(int, input().split()))
+print(number)
+
+n = int(input("input number : "))
+l = list()
+for i in range(n):
+    l.append(random.randint(1, 100))
+
+
 
