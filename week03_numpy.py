f60fa9b1e90b9aa0178538144443599866e7f690
202130119 <202130119@email.daelim.ac.kr>
2023-09-14 15:35:54 +0900

    week03 v1.3 2d array 인데 오류 발생

--- week03_numpy.py
+++ week03_numpy.py
@@ -18,18 +18,17 @@ import random
 import tkinter as tk  # Built in GUI
 from tkinter import messagebox #팝업 윈도우
 
+def create_2d_array(row, col):
+    matrix = np.random.randint(1, 101, size =(row, col))
+
 def click_button():
     try:
-        r = int(en_row.get())
-        c = int(en_col.get())
+        r, c = map(int, en_row_column.get().split())
 
-        rows = list()
-        for i in range(c):
-            rows = [[random.randint(1, 100) for i in range(r)]for i in range(c)]
+        rows = [[random.randint(1, 100) for i in range(r)]for i in range(c)]
         matrix = np.array(rows, dtype='int16')
         lbl_result.config(text=matrix) #출력 결과를 나타내는 레이블을 만듬
     except ValueError as err:
-        # lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
         messagebox.showerror('Error!',f'입력 값이 없습니다. \n{err}')
         # messagebox.showwarning('Error!', f'입력 값이 없습니다. \n{err}')
 
@@ -39,15 +38,17 @@ window.geometry('300x150')
 
 # create widget, 작성한 순서에 따라 ui가 구성
 lbl_result = tk.Label(text="random numpy 2D array")
-en_row = tk.Entry() #입력 상자
-en_col = tk.Entry()
+en_row_column = tk.Entry() #입력 상자
 btn_click = tk.Button(text="click me!", command=click_button)
 
 # widget layout
-lbl_result.grid(row=0, column=0, columnspan=2)
-en_row.grid(row=1, column=0)
-en_col.grid(row=1, column=1)
-btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
+lbl_result.pack()
+en_row_column.pack(fill='x')
+btn_click.pack(fill='x')
+# lbl_result.grid(row=0, column=0, columnspan=2)
+# en_row.grid(row=1, column=0)
+# en_col.grid(row=1, column=1)
+# btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
 # 격자배열
 # lbl_result.pack()
 # en_row.pack(fill='x')
