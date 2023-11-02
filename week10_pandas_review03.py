476ffccdc3d5cbf1b90cca14bb62320006355ee4
202130119 <202130119@email.daelim.ac.kr>
2023-11-02 15:40:57 +0900

    week10

--- week10_pandas_review03.py
+++ week10_pandas_review03.py
@@ -6,3 +6,13 @@ a2 = np.full((3,3), 5)
 print(a2)
 a3 = np.eye(3) #단위 행렬
 print(a3)
+
+a4 = a3[:2, 1:]
+print(a4)
+
+a5 = a3[2]
+a6 = a3[1, :]
+a7 = a3[2:, :]
+#결국 다 같은 값
+
+print(a7)
---
+++ week10_pandas_review04.py
@@ -0,0 +1,7 @@
+import numpy as np
+a1 = np.arange(1, 6)
+a2 = np.array([6, 7, 8, 9, 10])
+print(a1 + a2)
+print(a1 - a2)
+print(a1 * a2)
+print(np.dot(a1, a2))
