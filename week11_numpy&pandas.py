62ec1228beb1519eb4a8ac05e20932d620a331e3
202130119 <202130119@email.daelim.ac.kr>
2023-11-09 15:23:34 +0900

    week10

---
+++ week11_numpy&pandas.py
@@ -0,0 +1,107 @@
+# import pandas as pd
+#
+# df1 = pd.DataFrame(
+#     {
+#         "KOR": [99, 91, 100],
+#         "ENG": [89, 98, 97],
+#         "MAT": [100, 90, 85],
+#     }, index=[1, 2, 3]
+# )
+# print(df1)
+#
+# df2 = pd.DataFrame(
+#     [[99, 89, 100],
+#         [91, 98, 90],
+#         [100, 97, 85]],
+#     index=[1, 2, 3],
+#     columns=['KOR', 'ENG', 'MAT']
+# )
+# print(df2)
+
+# import pandas as pd
+# df1 = pd.DataFrame(
+#     {
+#         "KOR": [99, 91, 100],
+#         "ENG": [89, 98, 97],
+#         "MAT": [100, 90, 85],
+#     }, index=[1, 2, 3]
+# )
+# # print(df1)
+#
+# df2 = pd.DataFrame(
+#     [[99, 89, 100],
+# @@ -16,4 +16,14 @@
+#     index=[1, 2, 3],
+#     columns=['KOR', 'ENG', 'MAT']
+# )
+# print(df2)
+# df2 = (pd.melt(df2)
+#        .rename(columns={
+#         'variable': 'subject',
+#         'value': 'score'})
+#        .query('score >= 90')
+#        )
+#
+# print(df2)
+
+# import pandas as pd
+# df1 = pd.DataFrame(
+#     {
+#         "KOR": [99, 91, 100],
+#         "ENG": [89, 98, 97],
+#         "MAT": [100, 90, 85],
+#     }, index=[1, 2, 3]
+# )
+# print(df1)
+# print(df1.sort_values('MAT'))
+#
+# df2 = pd.DataFrame(
+#     [[99, 89, 100],
+#         [91, 98, 90],
+#         [100, 97, 85]],
+#     index=[1, 2, 3],
+#     columns=['KOR', 'ENG', 'MAT']
+# )
+# print(df2)
+# df2 = (pd.melt(df2)
+#        .rename(columns={
+#         'variable': 'subject',
+#         'value': 'score'})
+#        .query('score >= 90')
+#        .sort_values('score', ascending=False)
+#        )
+#
+# print(df2)
+
+# import pandas as pd
+# df1 = pd.DataFrame(
+#     {
+#         "KOR": [99, 91, 100],
+#         "ENG": [89, 98, 97],
+#         "MAT": [100, 90, 85],
+#     }, index=[1, 2, 3]
+# )
+# print(df1)
+# print(df1.sort_values('MAT'))
+# df1 = df1.drop(columns=['ENG'])
+# print(df1)
+#
+# df2 = pd.DataFrame(
+#     [[99, 89, 100],
+#         [91, 98, 90],
+#         [100, 97, 85]],
+#     index=[1, 2, 3],
+#     columns=['KOR', 'ENG', 'MAT']
+# )
+# print(df2)
+# df2 = (pd.melt(df2)
+#        .rename(columns={
+#         'variable': 'subject',
+#         'value': 'score'})
+#        .query('score >= 90')
+#        .sort_values('score', ascending=False)
+#        )
+# print(df2)
+
+
+
