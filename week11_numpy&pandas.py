9577525d48949c818369a752750a3231de77d9e9
202130119 <202130119@email.daelim.ac.kr>
2023-11-09 15:26:36 +0900

    week11 v5.1~5.5

--- week11_numpy&pandas.py
+++ week11_numpy&pandas.py
@@ -103,5 +103,33 @@
 #        )
 # print(df2)
 
+# import pandas as pd
+# df = pd.DataFrame(
+#     [[99, 89, 100],
+#         [91, 98, 90],
+#         [100, 97, 85],
+#         [83, 100, 94]],
+#     index=[1, 2, 3, 4],
+#     columns=['KOR', 'ENG', 'MAT']
+# )
+# print(df)
+# # df_copy = df.iloc[:, [0, 2]]
+# # df_copy = df.loc[:, ['KOR', 'MAT']]
+# df_copy = df.loc[:, 'KOR':'MAT':2]
+# print(df_copy)
+#
+# df = (pd.melt(df)
+#        .rename(columns={
+#         'variable': 'subject',
+#         'value': 'score'})
+#        .query('score >= 90')
+#        .sort_values('score', ascending=False)
+#        )
+#
+# print(df)
+# print(df.iloc[:, [1]])
+
+
+
 
 
