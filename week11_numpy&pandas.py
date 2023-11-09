4abbef26fe1b7390f356a04b23a7e99942d3cca0
202130119 <202130119@email.daelim.ac.kr>
2023-11-09 15:34:05 +0900

    week11 v5.6

--- week11_numpy&pandas.py
+++ week11_numpy&pandas.py
@@ -1,135 +1,31 @@
-# import pandas as pd
-#
-# df1 = pd.DataFrame(
-#     {
-#         "KOR": [99, 91, 100],
-#         "ENG": [89, 98, 97],
-#         "MAT": [100, 90, 85],
-#     }, index=[1, 2, 3]
-# )
-# print(df1)
-#
-# df2 = pd.DataFrame(
-#     [[99, 89, 100],
-#         [91, 98, 90],
-#         [100, 97, 85]],
-#     index=[1, 2, 3],
-#     columns=['KOR', 'ENG', 'MAT']
-# )
-# print(df2)
+import pandas as pd
+df = pd.DataFrame(
+    [[99, 89, 100],
+        [91, 98, 90],
+        [100, 97, 85],
+        [83, 100, 94]],
+    index=[1, 2, 3, 4],
+    columns=['KOR', 'ENG', 'MAT']
+)
+print(df)
+# df_copy = df.iloc[:, [0, 2]]
+# df_copy = df.loc[:, ['KOR', 'MAT']]
+df_copy = df.loc[:, 'KOR':'MAT':2]
+# 1번 학생의 수학 성적(100) 출력
+print(df.at[1, 'MAT'])
+#print(df.iat[1, 3])
+print(df.iat[0, 2])
+# 국어, 수학 칼럼을 추출
+# 조건은 국어 성적이 95점 이상인 경우
+df_copy = df.loc[df['KOR']>=95, ['KOR', 'MAT']]
+print(df_copy)
 
-# import pandas as pd
-# df1 = pd.DataFrame(
-#     {
-#         "KOR": [99, 91, 100],
-#         "ENG": [89, 98, 97],
-#         "MAT": [100, 90, 85],
-#     }, index=[1, 2, 3]
-# )
-# # print(df1)
-#
-# df2 = pd.DataFrame(
-#     [[99, 89, 100],
-# @@ -16,4 +16,14 @@
-#     index=[1, 2, 3],
-#     columns=['KOR', 'ENG', 'MAT']
-# )
-# print(df2)
-# df2 = (pd.melt(df2)
-#        .rename(columns={
-#         'variable': 'subject',
-#         'value': 'score'})
-#        .query('score >= 90')
-#        )
-#
-# print(df2)
-
-# import pandas as pd
-# df1 = pd.DataFrame(
-#     {
-#         "KOR": [99, 91, 100],
-#         "ENG": [89, 98, 97],
-#         "MAT": [100, 90, 85],
-#     }, index=[1, 2, 3]
-# )
-# print(df1)
-# print(df1.sort_values('MAT'))
-#
-# df2 = pd.DataFrame(
-#     [[99, 89, 100],
-#         [91, 98, 90],
-#         [100, 97, 85]],
-#     index=[1, 2, 3],
-#     columns=['KOR', 'ENG', 'MAT']
-# )
-# print(df2)
-# df2 = (pd.melt(df2)
-#        .rename(columns={
-#         'variable': 'subject',
-#         'value': 'score'})
-#        .query('score >= 90')
-#        .sort_values('score', ascending=False)
-#        )
-#
-# print(df2)
-
-# import pandas as pd
-# df1 = pd.DataFrame(
-#     {
-#         "KOR": [99, 91, 100],
-#         "ENG": [89, 98, 97],
-#         "MAT": [100, 90, 85],
-#     }, index=[1, 2, 3]
-# )
-# print(df1)
-# print(df1.sort_values('MAT'))
-# df1 = df1.drop(columns=['ENG'])
-# print(df1)
-#
-# df2 = pd.DataFrame(
-#     [[99, 89, 100],
-#         [91, 98, 90],
-#         [100, 97, 85]],
-#     index=[1, 2, 3],
-#     columns=['KOR', 'ENG', 'MAT']
-# )
-# print(df2)
-# df2 = (pd.melt(df2)
-#        .rename(columns={
-#         'variable': 'subject',
-#         'value': 'score'})
-#        .query('score >= 90')
-#        .sort_values('score', ascending=False)
-#        )
-# print(df2)
-
-# import pandas as pd
-# df = pd.DataFrame(
-#     [[99, 89, 100],
-#         [91, 98, 90],
-#         [100, 97, 85],
-#         [83, 100, 94]],
-#     index=[1, 2, 3, 4],
-#     columns=['KOR', 'ENG', 'MAT']
-# )
-# print(df)
-# # df_copy = df.iloc[:, [0, 2]]
-# # df_copy = df.loc[:, ['KOR', 'MAT']]
-# df_copy = df.loc[:, 'KOR':'MAT':2]
-# print(df_copy)
-#
-# df = (pd.melt(df)
-#        .rename(columns={
-#         'variable': 'subject',
-#         'value': 'score'})
-#        .query('score >= 90')
-#        .sort_values('score', ascending=False)
-#        )
-#
-# print(df)
-# print(df.iloc[:, [1]])
-
-
-
-
-
+df = (pd.melt(df)
+       .rename(columns={
+        'variable': 'subject',
+        'value': 'score'})
+       .query('score >= 90')
+       .sort_values('score', ascending=False)
+       )
+print(df)
+print(df.iloc[:, [1]])
