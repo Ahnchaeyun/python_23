1026c671c1c2f7851456a2e60adf9484e07f19c0
202130119 <202130119@email.daelim.ac.kr>
2023-11-09 16:30:14 +0900

    numpy pandas 사용 안하고 반복문 출력해보기

--- week11_numpy&pandas.py
+++ week11_numpy&pandas.py
@@ -7,36 +7,27 @@ df = pd.DataFrame(
     index=[1, 2, 3, 4],
     columns=['KOR', 'ENG', 'MAT']
 )
-# 각 과목별 평균 출력
-print(df.mean())
-# 각 과목별 최우수 성적 출력
-print(df.max())
 
-print(df)
-# 국어 성적과 영어 성적이 둘 다 95점 이상인 행을 추출
-print(df.query('KOR>=95 and ENG>=95'))
-# 1번 학생의 수학 성적(81) 출력
-print(df.at[1, 'MAT'])
-#print(df.iat[1, 3])  # error, 0행 0열 부터 시작한다. index기반 (label X)
-print(df.iat[0, 2])
-# 국어, 수학 칼럼을 추출
-# 조건은 국어 성적이 95점 이상인 경우
-df_copy = df.loc[df['KOR']>=95, ['KOR', 'MAT']]
-print(df_copy)
-
-
-def scale_score(n):
-    return n + 1
-# def scale_score(n):
-#     return n + 1
-#
-#
-# print(df)
-# df = df.apply(scale_score)
-# print(df)
-
-
-df = df.apply(scale_score)
 print(df)
 df = df.apply(lambda n: n + 1)
-print(df)
+
+df = [[99, 89, 81], [91, 98, 90], [95, 97, 85], [83, 96, 94]]
+index = [1, 2, 3, 4]
+columns = ['KOR', 'ENG', 'MAT']
+
+for j in columns:
+    print(j , end = ' ')
+print("\t")
+for i in range(len(df)):
+    print(f"{index[i]}   {df[i][0]}   {df[i][1]}   {df[i][2]}")
+
+# for c in columns:
+#     print(c, end = ' ')
+# print(' ')
+# for row in range(4):
+#     for col in range(3):
+#         print(df[row][col, end=' '])
+
+
+
+
