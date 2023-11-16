98f37093cbb87f6651195f71f390ce64408b3ff9
202130119 <202130119@email.daelim.ac.kr>
2023-11-16 16:33:41 +0900

    week12 hollys excel 예제

--- week12_web03.py
+++ week12_web03.py
@@ -1,43 +1,29 @@
-#파이썬에서 자료 다루는 것: 리스트, 딕셔너리
+import urllib.request
+
+import pandas
 from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
 
-html = """
-<html>
-<head>
-<title>스크레이핑 실습</title>
-</head>
-<body>
-<a href = "http://www.daelim.ac.kr">대림대학교</a><br>
-<a href = "http://www.harvard.edu">하버드대학교</a><br>
-</body>
-</html>
-"""
+shops = list()
 
-soup = BeautifulSoup(html, 'html.parser')
-urls = soup.find_all("a")
-print(urls)
+for i in range(1, 52):
+    url = f"https://www.hollys.co.kr/store/korea/korStore2.do?pageNo={i}&sido=&gugun=&store="
+    print(url)
+    page = urllib.request.urlopen(url) #다운로드 역할
+    soup = BeautifulSoup(page, 'html.parser') #soup 객체에 넣고 분석할 준비
+    tbody = soup.find('tbody')
+    trs = tbody.find_all('tr') #모든 tr 태그를 리스트로
 
-for url in urls:
-    print("{0}의 주소는 {1}입니다.".format(url.string, url.attrs['href']))
+    for tr in trs: #각 매장의 정보
+        tds = tr.find_all('td') #td 전부
+        shop_name = tds[1].string #매장 이름
+        shop_addr = tds[3].string #매장 주소
+        shop_tel = tds[5].string #매장 번호
 
+        shops.append([shop_name]+[shop_addr]+[shop_tel]+[datetime.datetime.now()]) #이차원 리스트 생성, 이차원 리스트로 만들어서 판다스를 사용하기 위해
 
-# from bs4 import BeautifulSoup
-# html = """
-# <html>
-# <head>
-# <title>스크레이핑 실습</title>
-# </head>
-# <body>
-# <h1 id="univ">대림대학교</h1>
-# <p>웹스크레이핑</p>
-# <p id="contents">파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
-# </body>
-# </html>
-# """
-# soup = BeautifulSoup(html, 'html.parser')
-# university = soup.find(id='univ')
-# contents = soup.find(id='contents')
-#
-# print(university)
-# print(contents)
-# print(contents.string)
+# print(shops)
+
+holly_df = pandas.DataFrame(shops, columns = ('매장명', '주소', '전화번호', '일시'))
+holly_df.to_csv('holly.csv', mode ="w", encoding='cp949')
