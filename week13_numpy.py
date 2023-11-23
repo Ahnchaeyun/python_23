8225d9a6764f7fa5a6bfe90977b2f2e61f514c0a
202130119 <202130119@email.daelim.ac.kr>
2023-11-23 16:14:59 +0900

    week13_titanic1

--- week13_numpy.py
+++ week13_numpy.py
@@ -1,42 +1,12 @@
+import seaborn as sns
 import pandas as pd
 
-d = pd.DataFrame(
-    [['persian', 'bengal', 'russian'],
-        ['maltese', 'poodle', 'retriever'],
-        ['maltese', 7, 'cute']],
-    index=['cat', 'dog', 'som'],
-    columns=[1, 2, 3]
-)
-print(d)
-print("-------------------")
+titanic = sns.load_dataset('titanic')
+# print(titanic.describe())#통계 수치
+# print(titanic.info())
+# print(titanic.head)
+# print(titanic.tail())
 
-data = {
-    'A': [99, 89, 81],
-    'B':[91, 98, 90],
-    'C':[95, 97, 85]
-}
-
-dd = pd.DataFrame(data)
-print(dd)
-
-value = dd[1:4] #인덱스 1에서 3까지의 행
-
-print(value)
-print("-------------------")
-
-df = pd.DataFrame(
-    [[99, 89, 81],
-        [91, 98, 90],
-        [95, 97, 85],
-        [83, 96, 94]],
-    index=[1, 2, 3, 4],
-    columns=['KOR', 'ENG', 'MAT']
-)
-
-data1 = df.loc[1:2] #1부터 2까지의 행
-data2 = df.iloc[1:4] # 정수/1부터 3까지의 행
-
-print(data1)
-print(data2)
-
-#apply: 특정함수 적용
+print(type(titanic))
+#결집체 정보 출력
+print(titanic.isnull())
