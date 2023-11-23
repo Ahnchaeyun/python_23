380b425ee65f9233e3bc1887d016db585f26b1a5
202130119 <202130119@email.daelim.ac.kr>
2023-11-23 16:31:36 +0900

    week13_titanic2

--- week13_numpy.py
+++ week13_numpy.py
@@ -2,11 +2,16 @@ import seaborn as sns
 import pandas as pd
 
 titanic = sns.load_dataset('titanic')
-# print(titanic.describe())#통계 수치
-# print(titanic.info())
-# print(titanic.head)
-# print(titanic.tail())
+#결측값 정보 출력, 15개 필드의 결측값
+# print(titanic.isnull().sum())
 
-print(type(titanic))
-#결집체 정보 출력
-print(titanic.isnull())
+print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
+# age 필드 값 출력, 결측 공간에 중앙값 넣기. inplace 넣어서 변경된 값을 적용
+
+print(titanic.tail())
+
+# titanic['deck'].fillna('Unknown', inplace=True)
+
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
+titanic['deck'].fillna('Unknown', inplace=True)
+print(titanic['deck'].tail(10))
