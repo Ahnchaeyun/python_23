97a0f8f534ad70dd67aa625e67cd0f4a7d9ee5c8
202130119 <202130119@email.daelim.ac.kr>
2023-11-30 15:04:08 +0900

    week14_v3

--- week13_numpy.py
+++ week13_numpy.py
@@ -1,17 +1,14 @@
+"""
+1) 생존자와 사망자 수를 구하시오.
+2) 남성과 여성의 생존률을 구하시오.
+"""
 import seaborn as sns
-import pandas as pd
-
 titanic = sns.load_dataset('titanic')
-#결측값 정보 출력, 15개 필드의 결측값
-# print(titanic.isnull().sum())
-
-print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
-# age 필드 값 출력, 결측 공간에 중앙값 넣기. inplace 넣어서 변경된 값을 적용
-
-print(titanic.tail())
-
-# titanic['deck'].fillna('Unknown', inplace=True)
-
-titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')
-titanic['deck'].fillna('Unknown', inplace=True)
-print(titanic['deck'].tail(10))
+# print(titanic.info())
+# print(titanic.head())
+male_survived = titanic[(titanic['survived'] == 1) & (titanic['sex'] == 'male')]['survived'].count()
+female_survived = titanic[(titanic['survived'] == 1) & (titanic['sex'] == 'female')]['survived'].count()
+male_count = titanic[(titanic['sex'] == 'male')]['sex'].count()
+female_count = titanic[(titanic['sex'] == 'female')]['sex'].count()
+print(male_survived, male_count, female_survived, female_count)
+print(male_survived/male_count, female_survived/female_count)
