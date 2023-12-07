fe9c39acf913b2f61ba00af3ff855199502f9624
202130119 <202130119@email.daelim.ac.kr>
2023-12-07 15:49:04 +0900

    week15_수정1

--- week09_numpy.py
+++ week09_numpy.py
@@ -1,12 +1,17 @@
 import numpy as np
 
+# numpy 배열
+# 1. 동일한 데이터 타입의 원소만 저장
+# 2. 메모리에 연속적으로 저장되어 빠른 속도로 데이터 처리 가능
+
 np_1d = np.arange(1, 10, 2)
-# np_1d = np.arange(1.0, 10.0, 2.0)
 print(np_1d)
 
 np_1d = np_1d.reshape(1, 5)  # reshape: 모양 바꾸는 것.
+# 생성한 배열의 모양을 1행 5열로 변경
 print(np_1d)
 
+#배열의 차원, shape, 원소의 개수를 출력하는 함수
 def info(x):
     print(f"배열의 차원은 {x.ndim} 입니다.")
     print(f"배열의 shape는 {x.shape} 입니다.")
@@ -14,22 +19,30 @@ def info(x):
 info(np_1d)
 
 print(np_1d, type(np_1d))
-print(np_1d, type(np_1d[0]))
+print(np_1d, type(np_1d[0])) #배열의 첫 번째 원소와 원소의 타입을 출력
 print(np_1d.ndim)
-print(np_1d.size)
-print(np_1d.shape)
+print(np_1d.size) #원소의 개수
+print(np_1d.shape) #원소의 모양
 
-# import numpy as np
-#
-# any_list = [1, "university", 3.91, (5, 7)]
-# converted_any_lise = ["1", "university", "3.91", "(5, 7)"]
-# # one_type_np = np.array[(2, 4, 6, 8)]
-# np_1d = np.array([2, 4, 6, 8])
-# np_2d = np.array([[2, 4], [6, 8]])
-# print(np_1d.ndim, np_2d.ndim)
-# print(np_1d.size, np_2d.size)
-# print(np_1d.shape, np_2d.shape)
-#
-# for i in any_list:
-#     print(i)
-#     # c++과 달리 데이터 타입이 다 달라도 출력 가능.
+np_1d = np.array([2, 4, 6, 8])
+np_2d = np.array([[2, 4], [6, 8]])
+print(np_1d.ndim, np_2d.ndim) #원소의 차원
+print(np_1d.size, np_2d.size)
+print(np_1d.shape, np_2d.shape)
+
+any_list = [1, "university", 3.91, (5, 7)]
+converted_any_list = ["1", "university", "3.91", "(5, 7)"] #원소를 문자열로 변환
+one_type_np_list = np.array(converted_any_list) #문자열로 변환된 리스트를 넘파이 배열로 변환
+# one_type_np_list = np.array(any_list)  # ValueError
+#문자열 외에 다른 타입의 튜플이 포함된 리스트를 넘파이로 변환하면 오류 발생
+
+for i in any_list:
+    print(i)
+
+for j in one_type_np_list:
+    print(j)
+
+
+for i in any_list:
+    print(i)
+    # c++과 달리 데이터 타입이 다 달라도 출력 가능.
--- week09_numpy_riview.py
+++ week09_numpy_riview.py
@@ -1,21 +1,45 @@
 import pandas as pd
 import numpy as np
 
+# pandas
+# 데이터 프레임을 생성(행과 열로 구성)
+# 행: 관칙치
+# 열: 변수
+# 1. head(), tail(): 데이터 프레임의 처음 몇 행 또는 마지막 몇 행 반환
+# 2. shape(): 행과 열의 개수 반환
+# 3. info(): 데이터 프레임 정보
+# 4. describe():  데이터 프레임 통계 정보
+
 df = pd.DataFrame({
     'a': [1, 2, np.nan, 4],
     'b':[5, np.nan, 7, 8],
     'c':[9, 10, 11, np.nan]
 })
+print("1")
+print(df)
 
+#df 객체의 결측값을 평균값으로 채운다
+#df.fillna(value = ) 에서 value 값으로 None, 1, 0의 결측값을 지정 가능
+#1.df.mean() 함수로 데이터프레임 객체의 각 열의 평균값 계산
+#2.df,fillna() 메서드로 열의 결측값을 평균값으로 채우기
+#3.inplace=True 옵션을 지정해 DataFrame 객체 자체를 수정
 df.fillna(df.mean(), inplace=True)
+print("2")
 print(df)
 
 for col in df.columns:
     # print(col)
     df[col] = np.where(pd.isnull(df[col]), np.mean(df[col]), df[col])
-    # 원본 데이터의 (데이터 프레임에) 평균값을 내고, 결집값? 들을 평균으로 채운다.
+    #np.where(조건, 참일 때 값, 거짓일 때 값)
+    #1. where 함수로 각 열의 원소가 결측값이면 평균 값, 아니면 원래 값
+    #2. pd.isnull()함수는 원소가 결측값인지 여부 확인
+print("3")
+print(df)
 
 from sklearn.impute import SimpleImputer #결집값 처리 전용 클래스
-i = SimpleImputer(strategy='mean')  #impute 객체
+i = SimpleImputer(strategy='mean')  
+#SimpleImputer 객체 생성
+#mean 옵션 지정시, 평균값으로 결측값을 처리
 df = pd.DataFrame(i.fit_transform(df), columns=df.columns)
+#df의 결측값을 SimpleImputer 객체를 사용해서 처리하고 새로운 데이터 프레임 객체 생성
 print(df)
